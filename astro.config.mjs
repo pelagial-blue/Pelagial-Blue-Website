1982e8616187436ef4df038e98497aad423cd69b
Robert Bewick <pelagial-blue@gmail.com>
2026-02-22 14:36:22 +0000

    Fix Cloudflare build; use static sitemap.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://pelagial-blue.com',
-  integrations: [sitemap()]
+  integrations: []
 });
